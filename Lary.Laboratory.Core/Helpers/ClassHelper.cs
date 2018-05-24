1cab067a22ef960c80fae0a6d6297628cbc3d11f
小醉魔 <xiaozuimo@live.com>
2018-05-24 13:57:28 +0800

    Add project files.

---
+++ Lary.Laboratory.Core/Helpers/ClassHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Lary.Laboratory.Core.Helpers
+{
+    /// <summary>
+    ///     Helper for class.
+    /// </summary>
+    public static class ClassHelper
+    {
+        /// <summary>
+        ///     Enumerate the subclasses of the type.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of class being enumerated.
+        /// </typeparam>
+        /// <returns>
+        ///     The subclasses.
+        /// </returns>
+        public static IEnumerable<Type> SubclassesOf<T>()
+            where T : class
+        {
+            return Assembly.GetAssembly(typeof(T))
+                           .GetTypes()
+                           .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(T)));
+        }
+
+        /// <summary>
+        ///     Create an instance for each subclass of the type.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of base class.
+        /// </typeparam>
+        /// <param name="constructorArgs">
+        ///     The constructor arguments.
+        /// </param>
+        /// <returns>
+        ///     The instances of subclasses.
+        /// </returns>
+        public static IEnumerable<T> CreateChildren4Type<T>(params object[] constructorArgs)
+            where T : class
+        {
+            var objects = new List<T>();
+
+            var children = SubclassesOf<T>();
+
+            foreach (var child in children)
+            {
+                objects.Add((T)Activator.CreateInstance(child, constructorArgs));
+            }
+
+            return objects;
+        }
+
+        /// <summary>
+        ///     Create an instance for each subclass of the type.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of base class.
+        /// </typeparam>
+        /// <param name="predicate">
+        ///     A function to test each element for a condition.
+        /// </param>
+        /// <param name="constructorArgs">
+        ///     The constructor arguments.
+        /// </param>
+        /// <returns>
+        ///     The instances of subclasses.
+        /// </returns>
+        public static IEnumerable<T> CreateChildren4Type<T>(Func<Type, bool> predicate, params object[] constructorArgs)
+            where T : class
+        {
+            var objects = new List<T>();
+
+            var types = SubclassesOf<T>().Where(predicate);
+
+            foreach (var type in types)
+            {
+                objects.Add((T)Activator.CreateInstance(type, constructorArgs));
+            }
+
+            return objects;
+        }
+    }
+}
---
+++ Lary.Laboratory.Core/Utils/CloneUtil.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lary.Laboratory.Core.Utils
+{
+    /// <summary>
+    ///     Provides methods for performing a deep copy of an object.
+    ///     Serialization is used to perform the copy.
+    /// </summary>
+    public static class CloneUtil
+    {
+        /// <summary>
+        ///     Perform a deep copy of the object.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of object being copied.
+        /// </typeparam>
+        /// <param name="source">
+        ///     The object instance to copy.
+        /// </param>
+        /// <returns>
+        ///     The copied object.
+        /// </returns>
+        public static T DeepClone<T>(T source)
+            where T : class
+        {
+            var clone = default(T);
+
+            // Don't serialize a null object, simply return the default for that object.
+            if (!Object.ReferenceEquals(source, null))
+            {
+                var json = JsonConvert.SerializeObject(source);
+
+                clone = JsonConvert.DeserializeObject<T>(json);
+            }
+            
+            return clone;
+        }
+    }
+}
